77f871d0b9fbee00c7dc01885d75b8c4bc151e11
Oscar Moreno <oscarpol95@gmail.com>
2025-06-24 15:54:54 -0500

    feat: update Tailwind CSS configuration and styles
    - Downgraded Tailwind CSS version to ^3.4.1 in package.json
    - Added postcss.config.js to configure Tailwind CSS and autoprefixer
    - Created a new logo.svg file for branding
    - Added Tailwind CSS directives to index.css for base, components, and utilities
    - Updated main.tsx to apply Tailwind CSS class for styling the header
    - Created tailwind.config.js for Tailwind CSS configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
-import React, { StrictMode } from 'react';
+import { StrictMode } from 'react';
 import ReactDom from 'react-dom/client';
 import './index.css';
 
 ReactDom.createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <h1>Hola mundo</h1>
+    <h1 className="text-6xl">Hola mundo</h1>
   </StrictMode>,
 )

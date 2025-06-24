984dca9cf3512c54a33e5e03afdb391db903851f
Oscar Moreno <oscarpol95@gmail.com>
2025-06-24 15:41:37 -0500

    refactor: remove unused files and update dependencies
    - Deleted unused SVG files (vite.svg, react.svg) from assets.
    - Removed App.css and App.tsx as they were no longer needed.
    - Cleaned up index.css by removing unnecessary styles.
    - Updated main.tsx to render a simple "Hola mundo" message instead of the previous App component.
    - Added Tailwind CSS and PostCSS dependencies to package.json.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React, { StrictMode } from 'react';
+import ReactDom from 'react-dom/client';
+import './index.css';
 
-createRoot(document.getElementById('root')!).render(
+ReactDom.createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <h1>Hola mundo</h1>
   </StrictMode>,
 )
